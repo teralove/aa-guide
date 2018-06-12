302e58421e68680ff2298d3fd0a80ccf42bb0a8d
teralove <terascripts@gmail.com>
2018-06-12 18:58:30 -0400

    teralove update
    - Added messages for 1st and 2nd bosses
    - Added tank mode for Left/Right messages
    - Added more command arguments (on, off, tank)
    - Changed existing arguments to require 'aaguide' as command prefix.
    - Changed toggle command to work outside of dungeon.
    - Removed duplicate welcome messages while inside dungeon.
    - Updated README
    - Did not update manifest.json and module.json, so they were removed in this commit.

--- README.md
+++ README.md
@@ -3,24 +3,34 @@
 SCRIPT BY OWYN - For TERA
 Credits to teralove and soler91 and SHINOYX for their guides
 
-Call out attacks from Antaroth last boss including both NORMAL MODE & HARD MODE
+Call out attacks for all bosses including both NORMAL MODE & HARD MODE
 
 *Script automatically detects bosses and which dungeon hard or normal.
 
 Please report any issues to me either here or on discord :)
 
 # Commands 
-Only usable in the Antaroth Map. Using of commands outside map will return undefine command.
-- !aaguide to toggle module (DEFAULT: ON)
-- !toparty to toggle party notice, you must be the leader (DEFAULT: SELF ONLY (In notice chat))
-- !toparty stream - to toggle messages being sent only to proxy channel
+- **aaguide** - to toggle module (DEFAULT: ON)
+- **aaguide on** - to turn on module
+- **aaguide off** - to turn off module
+- **aaguide toparty** - to toggle party notice, you must be the leader (DEFAULT: SELF ONLY (In notice chat))
+- **aaguide stream** - to toggle messages being sent only to proxy channel (DEFAULT: OFF)
+- **aaguide tank** - to toggle Left/Right messages from Tank's POV. (DEFAULT: ON for Lancers and Brawlers, OFF for other classes)
 
 # Attacks announced
-- Front, Back stun
-- Back Stun
-- Right Safe , OUT safe (Right before the SPIN attack, normally you'd know which side is safe during the spin)
-- Left Safe , IN safe (Right before the SPIN attack, normally you'd know which side is safe during the spin)
+- First Boss (Ghergof)
+  - Out, In
+  - In, Out
+  - Delete soon
+- Second Boss (Kalioth)
+  - Spin
+  - Red, Out safe
+  - Blue, In safe
+- Third Boss (Antaroth)
+  - Front, Back stun
+  - Back Stun
+  - Right Safe , OUT safe (Right before the SPIN attack, normally you'd know which side is safe during the spin)
+  - Left Safe , IN safe (Right before the SPIN attack, normally you'd know which side is safe during the spin)
 
 # Notes
-- Left/Right message is announced from Boss POV (aka for DPS guys) - all guides do the same (rk9 guide for last boss for example), so tanks would have to do the opposite, it's not hard, just use brain.
 - To change\localize any message - edit index.js with notepad and then rename\delate module.json so it won't autoupdate to how it was before
--- index.js
+++ index.js
@@ -1,33 +1,73 @@
 /* Usable Sysbols ◎●←↑→↓↖↗↘↙ */
 
 const Command = require('command');
-const mapID = [9720, 9920];						// MAP ID to input [ Normal Mode , Hard Mode ]
+const mapID = [9720, 9920];					// MAP ID to input [ Normal Mode , Hard Mode ]
 
-const ThirdBossActions = {						// Third Boss Attack Actions
-	1188037721: {msg: 'Front, Back stun ↓'},
-	1188038721: {msg: 'Front, Back stun ↓'}, // rage
-	1188037719: {msg: 'Right Safe →'},
-	1188038719: {msg: 'Right Safe →'}, // rage
-	1188037717: {msg: '← Left Safe'},
-	1188038717: {msg: '← Left Safe'} // rage
+// BossAction[HuntingZoneId][TempalateId][Skill]
+const BossActions = {
+    // Normal Mode
+    720: {
+        // First Boss
+        1000: {
+            1188037724: {msg: 'Out ↓ In ↑'},
+            1188038724: {msg: 'Out ↓ In ↑'}, // rage
+            1188037725: {msg: 'In ↑ Out ↓'},
+            1188038725: {msg: 'In ↑ Out ↓'}, // rage
+            1188037908: {msg: 'Delete soon', deletionTimer: true}, // 'Your flesh will be deleted!'
+        },
+        // Second Boss NM
+        2000: {
+            // Second Boss NM
+            1188037714: {msg: 'Spin ↓'},
+            1188038714: {msg: 'Spin ↓'}, // rage
+            1188039727: {msg: 'Red, Out safe ↓'},
+            1188039828: {msg: 'Blue, In safe ↑'},   
+        },
+        // Third Boss NM
+        3000: {
+            // Third Boss NM
+            1188037721: {msg: 'Front, Back stun ↓'},
+            1188038721: {msg: 'Front, Back stun ↓'}, // rage
+            1188037719: {msg: '→ Right Safe →', msgForTanks: '← Left Safe ←'},
+            1188038719: {msg: '→ Right Safe →', msgForTanks: '← Left Safe ←'}, // rage
+            1188037717: {msg: '← Left Safe ←',  msgForTanks: '→ Right Safe →'},
+            1188038717: {msg: '← Left Safe ←',  msgForTanks: '→ Right Safe →'}, // rage
+            1188037712: {msg: 'Back stun ↓', checkTwoUp: true},
+            1188038712: {msg: 'Back stun ↓', checkTwoUp: true}, // rage            
+        }
+    },
+    // Hard Mode
+    920: {
+        // First Boss HM
+        1000: {
+            1201144938: {msg: 'In ↑ Out ↓'}, // (big aoe)
+            1201145938: {msg: 'Out ↓ In ↑'}, // (big aoe) rage
+            1201144924: {msg: 'Out ↓ In ↑'},
+            1201145924: {msg: 'Out ↓ In ↑'}, // rage
+            1201144925: {msg: 'In ↑ Out ↓'},
+            1201145925: {msg: 'In ↑ Out ↓'}, // rage
+            1201145108: {msg: 'Delete soon', deletionTimer: true},
+        },
+        // Second Boss HM
+        2000: {
+            1201144914: {msg: 'Spin ↓'},
+            1201145914: {msg: 'Spin ↓'}, // rage
+            1201147027: {msg: 'Red, Out safe ↓'},
+            1201147028: {msg: 'Blue, In safe ↑'},    
+        },
+        // Third Boss HM
+        3000: {
+            1201144921: {msg: 'Front, back stun ↓'},
+            1201145921: {msg: 'Front, back stun ↓'}, // rage
+            1201144919: {msg: '→ Right Safe, OUT safe', msgForTanks: '← Left Safe, OUT safe'},
+            1201145919: {msg: '→ Right Safe, OUT safe', msgForTanks: '← Left Safe, OUT safe'}, // rage
+            1201144917: {msg: '← Left Safe, IN safe',   msgForTanks: '→ Right Safe, IN safe'},
+            1201145917: {msg: '← Left Safe, IN safe',   msgForTanks: '→ Right Safe, IN safe'}, // rage
+            1201144912: {msg: 'Back stun ↓', checkTwoUp: true}, // HM
+            1201145912: {msg: 'Back stun ↓', checkTwoUp: true} // HM Rage
+        }
+    }
 };
-
-const ThirdBossActionsHM = {					// Third Boss Attack Actions Hard Mode
-	1201144921: {msg: 'Front, back stun ↓'},
-	1201145921: {msg: 'Front, back stun ↓'}, // rage
-	1201144919: {msg: 'Right Safe → , OUT safe'},
-	1201145919: {msg: 'Right Safe → , OUT safe'}, // rage
-	1201144917: {msg: '← Left Safe , IN safe'},
-	1201145917: {msg: '← Left Safe , IN safe'} // rage
-};
-
-const ThirdBossTwoUp = {
-	1188037712: {msg: 'Back stun ↓'},
-	1188038712: {msg: 'Back stun ↓'}, // rage
-	1201144912: {msg: 'Back stun ↓'}, // HM
-	1201145912: {msg: 'Back stun ↓'} // HM Rage
-};
-
 /*const ToTest = {						// Third Boss Attack Actions
 	1188037809: {msg: 'Red Aura ↓'},
 	1188038716: {msg: 'Red Thrust ↓'},
@@ -38,52 +78,76 @@ const ThirdBossTwoUp = {
 module.exports = function antaroth_guide(dispatch) {
 	const command = Command(dispatch);
 	let hooks = [],
-		sendToParty = false,
-		enabled = true,
-		insidemap = false,
-	   	streamenabled = false;
-		
-	dispatch.hook('S_LOAD_TOPO', 1, (event) => {
-		if (event.zone === mapID[0]) 
-		{								
-			insidemap = true;
-			command.message('Welcome to Antaroth - Normal Mode');
-			load();
-		} 
-		else if (event.zone === mapID[1]) {
-			insidemap = true;
-			command.message('Welcome to Antaroth - Hard Mode');
-			load();
-		} 
-		else
-		{
-			insidemap = false;
-			unload();
-		}
+        sendToParty = false,
+        enabled = true,
+        insidemap = false,
+        streamenabled = false,
+        isTank = false,
+        bossInfo = undefined;
+
+    dispatch.hook('S_LOGIN', 10, (event) => {
+        let job = (event.templateId - 10101) % 100;
+        isTank = (job === 1 || job === 10) ? true : false;
+    });
+    
+    dispatch.hook('C_MEET_BOSS_INFO', 1, (event) => {
+        bossInfo = event;
+    });
+    
+    dispatch.hook('S_LOAD_TOPO', 1, (event) => {
+        if (event.zone === mapID[0]) 
+        {								
+            if (!insidemap) command.message('Welcome to Antaroth - Normal Mode');
+            insidemap = true;
+            load();
+        } 
+        else if (event.zone === mapID[1]) {
+            if (!insidemap) command.message('Welcome to Antaroth - Hard Mode');
+            insidemap = true;
+            load();
+        } 
+        else
+        {
+            insidemap = false;
+            unload();
+        }
     });
 	
-	command.add('aaguide', () => {
-		if(!insidemap) { command.message('You must be inside Antaroth'); return; }
-		enabled = !enabled;
-		command.message('Antaroth Guide '+(enabled ? 'Enabled' : 'Disabled') + '.');
-	});
+    command.add('aaguide', (arg) => {
+        if (arg === undefined) {
+            //if(!insidemap) { command.message('You must be inside Antaroth'); return; }
+            enabled = !enabled;
+            command.message('Antaroth Guide '+(enabled ? 'Enabled' : 'Disabled') + '.');
+        }
+        else if(arg.toLowerCase() === "off")
+        {
+            enabled = false;
+            command.message('Antaroth Guide '+(enabled ? 'Enabled' : 'Disabled') + '.');
+        }
+        else if(arg.toLowerCase() === "on")
+        {
+            enabled = true;
+            command.message('Antaroth Guide '+(enabled ? 'Enabled' : 'Disabled') + '.');
+        }
+        else if(arg.toLowerCase() === "stream")
+        {
+            streamenabled = !streamenabled;
+            sendToParty = false;
+            command.message((streamenabled ? 'Stream mode Enabled' : 'Stream mode Disabled'));
+        }
+        else if(arg.toLowerCase() === "toparty")
+        {
+            streamenabled = false;
+            sendToParty = !sendToParty;
+            command.message((sendToParty ? 'Antaroth Guide - Messages will be sent to the party' : 'Antaroth Guide - Only you will see messages in chat'));
+        }
+        else if(arg.toLowerCase() === "tank")
+        {
+            isTank = !isTank;
+            command.message('Antaroth Guide - Tank Mmde '+(enabled ? 'Enabled' : 'Disabled') + '.');
+        }
+    });
 	
-	command.add('toparty', (arg) => {
-		if(!insidemap) { command.message('You must be inside Antaroth'); return; }
-		if(arg === "stream")
-		{
-			streamenabled = !streamenabled;
-			sendToParty = false;
-			command.message((streamenabled ? 'Stream mode Enabled' : 'Stream mode Disabled'));
-		}
-		else
-		{
-			streamenabled = false;
-			sendToParty = !sendToParty;
-			command.message((sendToParty ? 'Antaroth Guide - Messages will be sent to the party' : 'Antaroth Guide - Only you will see messages in chat'));
-		}
-	});
-
 	function sendMessage(msg)
 	{
 		if (sendToParty) 
@@ -107,31 +171,42 @@ module.exports = function antaroth_guide(dispatch) {
 		}
 	}
 	
-	let lasttwoup = 0;
-	function load()
-	{
-		if(!hooks.length)
-		{
-			hook('S_ACTION_STAGE', 5, (event) => {
-				if(!enabled || event.templateId !== 3000) return;
-				
-				if (ThirdBossActions[event.skill])
-				{
-					sendMessage(ThirdBossActions[event.skill].msg);
-				}
-				else if (ThirdBossActionsHM[event.skill])
-				{
-					sendMessage(ThirdBossActionsHM[event.skill].msg);
-				}
-				else if (ThirdBossTwoUp[event.skill])
-				{
-					let now = Date.now();
-					if((now - lasttwoup) < 3000) // usually <2350
-					{
-						sendMessage(ThirdBossTwoUp[event.skill].msg /* + " : " + String(now - lasttwoup)*/ );
-					}
-					lasttwoup = now;
-				}
+    let lasttwoup = 0;
+    function load()
+    {
+        if(!hooks.length)
+        {
+            hook('S_ACTION_STAGE', 5, (event) => {              
+                if (!enabled) return;                
+                /*Optimization Todo: Skip players, only continue if boss is performing action*/
+                
+                let bossAction = BossActions[bossInfo.huntingZoneId][bossInfo.templateId][event.skill];
+                if (bossAction) 
+                {
+                    if (bossAction.deletionTimer) 
+                    {
+                        /*Bug: Deletes start happening at 80%, the first notification will not be sent. To fix, need to check boss HP*/
+                        /*Bug: A message gets sent one time after boss dies. To fix, need to clear timer when boss dies or when party wipes.*/
+                        setTimeout(()=>{sendMessage(bossAction.msg)}, 60000); // usually happens ~70000
+                    }
+                    else if (bossAction.checkTwoUp) 
+                    {
+                        let now = Date.now();
+                        if((now - lasttwoup) < 3000) // usually <2350
+                        {
+                            sendMessage(bossAction.msg /* + " : " + String(now - lasttwoup)*/ );
+                        }
+                        lasttwoup = now;
+                    }
+                    else if (isTank && bossAction.msgForTanks) 
+                    {
+                        sendMessage(bossAction.msgForTanks);
+                    }
+                    else
+                    {
+                        sendMessage(bossAction.msg);
+                    }
+                }
 				/*else if (ToTest[event.skill])
 				{
 					sendMessage(ToTest[event.skill].msg);
@@ -143,9 +218,9 @@ module.exports = function antaroth_guide(dispatch) {
 					console.log(h + ":" + m + ":" + s + " - " + JSON.stringify(event, null, 4));
 					console.log(ToTest[event.skill].msg);
 				}*/
-			});
-		}
-	}
+            });
+        }
+    }
 	
 	function unload() {
 		if(hooks.length) {
